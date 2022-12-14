387fb644f8c2baedc5a375153c8671d9d71e8cbd
Brokebor <120442928+Brokebor@users.noreply.github.com>
2022-12-13 23:54:24 -0500

    Add files via upload

--- Project3/script copy.js
+++ Project3/script copy.js
@@ -1,47 +1,20 @@
-var doc= document;
-const array=document.getElementsByTagName("button");
 
-/*let arr2=['b1','b2','b3','b4','b5','b6','b7','b8','b9'];*/
-let board =["", "", "", "", "", "", "", "", ""];
-var drawPoint =9
+let board=["","","","","","","","",""];
 const boardIndex =['b1','b2','b3','b4','b5','b6','b7','b8','b9']
-
-
-
+const arr1=document.querySelectorAll(".button");
 let playerTurn= document.getElementById('turn');
-for (var i=0; i <array.length;i++){
-   
-   console.log(array[i]);
-  let  boardQ = (array[i]);
-  console.log(boardQ)
-   boardQ.addEventListener("click",testClick);
-  boardQ.addEventListener("click",buttonClick);
-  
-   /*boardQ.addEventListener("click",checkPlayer);*/
-   
-   
 
- /*var boardStick=board[i];*/
- /*boardStick.addEventListener("click",unpdateThing)*/
-}
-
-/*console.log(board);*/
-console.log(boardQ);
-var player1 ="X";
-var player2 ="0";
+let currentPlayer= "X";
+let gameOn=false;
 
 
-let currentPlayer= player1;
-let gameOn=true;
 
-console.log(gameOn);
 
-console.log(currentPlayer);
-
+console.log(arr1);
 console.log(playerTurn);
 
 
-const winningConditon=[
+const winningCondition = [
 [0,1,2],
 [3,4,5],
 [6,7,8],
@@ -51,52 +24,79 @@ const winningConditon=[
 [0,4,8],
 [2,4,6],
 ];
-startGame();
-
-function startGame(){
-   
-  
-   let currTurn= document.getElementById("turn");
-   currTurn.innerText = currentPlayer + "'s turn";
-   console.log(boardQ)
-
-  
-   
+gameIsOn();
+function buttonClick(id) {
+   const clicked = this.document.getElementById(id)
+   console.log(id)
+   if (clicked.textContent != "" || !gameOn) {
+       return;
    }
-  function buttonClick(){
-   const boardIndex=array;
-   console.log(boardIndex);
-   if(board[boardIndex]!=""||!gameOn){
-      return;
+   else {
+       clicked.textContent = currentPlayer
+       isWinAndFin();
+       if (currentPlayer == "X") {
+           currentPlayer = "O"
+       }
+       else { currentPlayer = "X" }
+       playerturn = document.getElementById("turn").textContent = currentPlayer + "'s turn"
+       //updateSpace(this,arr1);
    }
-   
-   testClick(document,boardIndex)
+}
+/*function checkDraw(){
+   for( let bean = 0; bean<= arr1.length; bean++){
+      if(bean=9){
+         console.log("draw");
+
+
+         return playerTurn.innerHTML = "draw";
+      } 
+      
+      return ;
+}
+}*/
+function greet(){
+   console.log(winningCondition);
+
+
+}
+greet();
+/*function isWin() {
+   return winningCondition.some(
+   (condition) => {
+   let line = condition.map((idx)=>document.getElementById(boardIndex[idx]).textContent);
+   return (line[0] != "" && line.every((x)=>x==line[0]))
+   }
+   )
+   }*/
+   /*let gameWon=false;
+   if (!gameWon){
+      isWinAndFin(false);
+   }*/
+   function isWinAndFin(){
+      let gameWon = false;
+      let draw = false;
+      winningCondition.map(condition=>{
+          if (board[condition[0]] == board[condition[1]] && board[condition[1]] == board[condition[2]] && board[condition[0]] != ""){
+              gameWon = true;
+              console.log("win");
+          }else if (board.every((x)=>x!="")){
+              draw = true;
+              console.log("draw");
+          }
+      })
+      return [gameWon,draw];
   }
 
-function testClick(board,i) {
-   console.log("x");
-   board[i]=currentPlayer;
-  
 
-   
-  board.innerText=currentPlayer;
-   
-   
-   /*const cell=event.target;*/
-  /*document.write ("X");*/
- 
-  /*placeMark(cell,playerChoice)*/
- 
 
-  
-   
+function gameIsOn(){
+   playerTurn.textContent= currentPlayer +" 's turn";
+   //board.forEach(function (_arr1) {
+   //      return board.addEventListener("click", updateSpace);
+   //   });
+   gameOn=true;
+   //checkDraw();
+
+
+
 }
-/*placeMark(cell,playerChoice);{
-   cell.classList.add(playerChoice)
-}
-
-/*function add(){
-   let=newElement =document.createElement("div")
-   document,getElementById("div2").appendChild(newElement);*/
-
-
--- Project3/script.js
+++ Project3/script.js
@@ -25,95 +25,140 @@ const winningCondition = [
 [2,4,6],
 ];
 gameIsOn();
-function buttonClick(id){
+function buttonClick(id) {
    const clicked = this.document.getElementById(id)
-    console.log(id)
-   if (clicked.textContent != "" || !gameOn){
-      return;
+   console.log(id)
+   if (clicked.textContent != "" || !gameOn) {
+       return;
    }
-   else{
-      clicked.textContent = currentPlayer
+   else {
+       clicked.textContent = currentPlayer
+       isWinAndFin();
        if (currentPlayer == "X") {
-            currentPlayer = "O"
+           currentPlayer = "O"
        }
-       else {currentPlayer = "X"}
-       playerturn=document.getElementById("turn").textContent = currentPlayer + "'s turn"
-      //updateSpace(this,arr1);
-      isWinAndFin();
+       else { currentPlayer = "X" }
+       playerturn = document.getElementById("turn").textContent = currentPlayer + "'s turn"
+       //updateSpace(this,arr1);
    }
+}
+function checkDraw(){
+   for( let bean = 0; bean<= arr1.length; bean++){
+      if(bean=9){
+         console.log("draw");
+
+
+         return playerTurn.innerHTML = "draw";
+      } 
+      
+      return ;
+}
 }
 function greet(){
    console.log(winningCondition);
 
+
 }
 greet();
-function isWinAndFin(){
-   let gameWon=false;
-
-   for (let i=0;i<winningCondition.length;i++){
-      const win=winningCondition[i];
-     /* const clickA= board[win[0]];
-      const clickB=board[ win[1]];
-      const clickC =board[win[2]];
-      
-     
-     /* if(clickA=="" ||clickB=="" ||clickC=="" ){
-         continue;
-      }
-      if(clickA== clickB== clickB == clickC){
-         gameWon=true;
-         break;
-      }
+/*function isWin() {
+   return winningCondition.some(
+   (condition) => {
+   let line = condition.map((idx)=>document.getElementById(boardIndex[idx]).textContent);
+   return (line[0] != "" && line.every((x)=>x==line[0]))
    }
-   if(gameWon){
-      playerTurn.innerText = currentPlayer + " won";
-      gameIsOn=false;
+   )
    }*/
+   /*let gameWon=false;
+   if (!gameWon){
+      isWinAndFin(false);
+   }*/
+function isWinAndFin(){
+   
+   
+   
 
-  // for (var i=0;i<= 9; i++){
+   for (var i=0;i<=winningCondition.length; i++){
+      winningCondition[i];
+      let winTrue =true;
 
-   if(winningCondition[0]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+      //let winTrue=true;
+      console.log(winningCondition[0]);
+      
+     const  winAnno=playerTurn.innerHTML = currentPlayer + "'s Wins!";
+      
+
+
+
+   if(winningCondition[0]==winTrue){
+      console.log("win");
+      wintrue=gameWon;
+      gameOn=false;
+
+
+      return winAnno;
       
    }
-   else if(winningCondition[1]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[1]==winTrue){
+      console.log("win");
+
+      return winAnno;
       
    }
-else if(winningCondition[2]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+else if(winningCondition[2]==winTrue){
+   console.log("win");
+
+   return winAnno;
       
    }
-   else if(winningCondition[3]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[3]==winTrue){
+      console.log("win");
+
+      return winAnno;
       
    }
-   else if(winningCondition[4]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[4]==winTrue){
+      console.log("win");
+
+      return winAnno;
       
    }
-   else if(winningCondition[5]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[5]==winTrue){
+      console.log("win");
+
+      return winAnno;
       
    }
-   else if(winningCondition[6]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[6]==winTrue){
+      console.log("win");
+
+      return winAnno;
       
    }
-   else if(winningCondition[7]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   else if(winningCondition[7]==winTrue){
+      console.log("win");
+      return winAnno;
       
    }
-   else if(winningCondition[8]==true){
-      this.document.getElementById('turn').innerHTML = currentPlayer + "'s Wins!"
+   
       
-   }
-   //else{
-   //   this.document.getElementById('turn').innerHTML = "draw"
+   
+      //else{
+         
+         /*wintrue=false
+      //while( let g = 0; g< board.length; g++;){
+      console.log("draw")
+      return playerTurn.innerHTML = "draw";}*/
+
+   return 
+   
    //}
+
+
+   
+}
 }
 
-}
+
 
 
 function gameIsOn(){
@@ -122,8 +167,8 @@ function gameIsOn(){
    //      return board.addEventListener("click", updateSpace);
    //   });
    gameOn=true;
+   checkDraw();
 
 
 
 }
-
